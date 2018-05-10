f5d8e2b66fe0be30764a44aa468f9c589a7544e0
Compiler Exe <compilerexe@gmail.com>
2018-05-10 18:54:28 +0700

    read devices from firebase.

--- vue-dashboard/src/components/LeftPanel.vue
+++ vue-dashboard/src/components/LeftPanel.vue
@@ -7,9 +7,11 @@
       <li>
         <a class="is-active">Devices</a>
         <ul>
-          <li><a>PAO-003</a></li>
-          <li><a>PAO-002</a></li>
-          <li><a>PAO-001</a></li>
+          <li v-for="device in devices">
+            <a href="#">
+              {{ device.name}}
+            </a>
+          </li>
         </ul>
       </li>
     </ul>
@@ -17,20 +19,32 @@
 </template>
 
 <script>
-  // import * as firebase from 'firebase'
-  //
-  // let app = firebase.initializeApp({
-  //   apiKey: 'AIzaSyDjKtk-mfuS_g3ZnWI4NX2x5ynUQU1Y3og',
-  //   authDomain: 'performance-182414.firebaseapp.com',
-  //   databaseURL: 'https://performance-182414.firebaseio.com',
-  //   projectId: 'performance-182414',
-  //   storageBucket: 'performance-182414.appspot.com',
-  //   messagingSenderId: '779931616641'
-  // })
-  //
-  // console.log(app.database().ref('/'))
+  import * as firebase from 'firebase'
+
+  let devices = []
+
+  let app = firebase.initializeApp({
+    apiKey: 'AIzaSyDjKtk-mfuS_g3ZnWI4NX2x5ynUQU1Y3og',
+    authDomain: 'performance-182414.firebaseapp.com',
+    databaseURL: 'https://performance-182414.firebaseio.com',
+    projectId: 'performance-182414',
+    storageBucket: 'performance-182414.appspot.com',
+    messagingSenderId: '779931616641'
+  })
+
+  app.database().ref('/').once('value', (snapshot) => {
+    snapshot.forEach(child => {
+      devices.push(child.val())
+      console.log(devices)
+    })
+  })
 
   export default {
-    name: 'LeftPanel'
+    name: 'LeftPanel',
+    data () {
+      return {
+        devices: devices
+      }
+    }
   }
 </script>
