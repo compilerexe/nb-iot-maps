6134ddd342ae57b41eaff717db3929c94c2f8aeb
Compiler Exe <compilerexe@gmail.com>
2018-05-10 17:47:35 +0700

    mockup design.

--- vue-dashboard/src/App.vue
+++ vue-dashboard/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
+    <div class="container">
+      <div class="section">
+        <div class="columns">
+          <div class="column is-3">
+            <LeftPanel/>
+          </div>
+          <div class="column">
+            <RightPanel/>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import 'bulma/css/bulma.min.css'
+  import LeftPanel from './components/LeftPanel'
+  import RightPanel from './components/RightPanel'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      LeftPanel,
+      RightPanel
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+    /*margin-top: 60px;*/
+  }
 </style>
---
+++ vue-dashboard/src/components/RightPanel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+
+  function initMap() {
+    let uluru = {lat: 15.8700, lng: 100.9925};
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 6,
+      center: uluru
+    });
+
+    let contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+    let infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    let marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      title: 'Uluru (Ayers Rock)'
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
+
+  export default {
+    name: 'RightPanel',
+    mounted: function () {
+      initMap()
+    }
+  }
+</script>
+
+<style>
+  #map {
+    width: 100%;
+    height: 100%;
+  }
+</style>
