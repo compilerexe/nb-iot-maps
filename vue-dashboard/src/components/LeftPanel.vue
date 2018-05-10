9271cf26dd770720c81ca7b337e5f47d1109becf
Compiler Exe <compilerexe@gmail.com>
2018-05-10 18:11:50 +0700

    update google maps.

--- vue-dashboard/src/components/LeftPanel.vue
+++ vue-dashboard/src/components/LeftPanel.vue
@@ -17,6 +17,19 @@
 </template>
 
 <script>
+  // import * as firebase from 'firebase'
+  //
+  // let app = firebase.initializeApp({
+  //   apiKey: 'AIzaSyDjKtk-mfuS_g3ZnWI4NX2x5ynUQU1Y3og',
+  //   authDomain: 'performance-182414.firebaseapp.com',
+  //   databaseURL: 'https://performance-182414.firebaseio.com',
+  //   projectId: 'performance-182414',
+  //   storageBucket: 'performance-182414.appspot.com',
+  //   messagingSenderId: '779931616641'
+  // })
+  //
+  // console.log(app.database().ref('/'))
+
   export default {
     name: 'LeftPanel'
   }
--- vue-dashboard/src/components/RightPanel.vue
+++ vue-dashboard/src/components/RightPanel.vue
@@ -52,6 +52,7 @@
       initMap()
     }
   }
+
 </script>
 
 <style>
